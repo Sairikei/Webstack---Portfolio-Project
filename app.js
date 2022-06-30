58de5fab17430bf2f3a40fe37f72c667fd4dc716
Sairikei <gedionsaiyuah@gmail.com>
2022-06-30 07:38:17 +0300

    touches

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ arrows.forEach((arrow,i)=>{
     const itemNumber = movieLists[i].querySelectorAll("img").length;
     let clickCounter = 0;
     arrow.addEventListener("click",()=>{
+        const ratio = Math.floor(window.innerWidth / 270);
         clickCounter++;
-        if (itemNumber - (4 + clickCounter) >= 0){
+        if (itemNumber - (4 + clickCounter) + (4 - ratio) >= 0){
             movieLists[i].style.transform = `translateX(${
                 movieLists[i].computedStyleMap().get("transform")[0].x.value -300
             }px)`
@@ -15,8 +16,11 @@ arrows.forEach((arrow,i)=>{
             clickCounter = 0;
         }  
     });
+
+    console.log(Math.floor(window.innerWidth / 270));
 });
 
+//Toggle
 
 const ball = document.querySelector(".toggle-ball");
 const items = document.querySelectorAll(
